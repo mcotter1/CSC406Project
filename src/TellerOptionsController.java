c5862deaf0f21f519cb86ea75f671e5ee19dc5ca
rapid131 <thutton2@missouriwestern.edu>
2024-04-14 20:44:28 -0500

    some changes

--- src/TellerOptionsController.java
+++ src/TellerOptionsController.java
@@ -1,17 +1,29 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TellerOptionsController {
+public class TellerOptionsController implements Initializable {
     private Stage stage; // This is the stage for the scene
     private Scene scene; // This is the scene for the stage
     private static Parent root; // This is the root for the scene
+    @FXML
+    private Label accountlabel;
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        accountlabel.setText(App.Customers.get(App.currentcustomerindex).getAccounts().get(App.currentaccountindex).toString());
+    }
     @FXML
     void TellerBtnClicked(ActionEvent event) throws Exception {
         root = FXMLLoader.load(getClass().getResource("TellerSelectAccount.fxml"));
